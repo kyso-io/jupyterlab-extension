ff0673b45558a7582f6e6c752957971e284355af
eoinmurray <amonecho1@gmail.com>
2018-11-26 19:49:22 +0100

    auto include server ext

--- setup.py
+++ setup.py
@@ -1,21 +1,36 @@
 """
-Setup Module to setup Python Handlers (Git Handlers) for the Git Plugin.
+Setup module for the jupyterlab_github proxy extension
 """
 from setuptools import setup, find_packages
+import json
+from setupbase import (
+    create_cmdclass, ensure_python, find_packages
+    )
+
+data_files_spec = [
+    ('etc/jupyter/jupyter_notebook_config.d',
+     'jupyter-config/jupyter_notebook_config.d', 'kyso_jupyterlab.json'),
+]
+
+cmdclass = create_cmdclass(data_files_spec=data_files_spec)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+f = open('./package.json', 'r')
+pkg = json.loads(f.read())
+f.close()
+
 setup(
     name='kyso_jupyterlab',
-    version='0.4.4',
+    version=pkg['version'],
+    cmdclass=cmdclass,
     author='Git Intern Team, Noah Stapp, Jenna Landy, Alena Mueller',
     description="A server extension for JupyterLab's git extension",
     long_description=long_description,
     packages=find_packages(),
     install_requires=[
-        'notebook',
-        'psutil'
+        'notebook'
     ],
     package_data={'kyso_jupyterlab': ['*']},
 )
